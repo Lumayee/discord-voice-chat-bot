af1d0ae3ef00a692879cba524bc8aaa5317335a9
Paul Weber <Loominux@posteo.com>
2023-12-25 19:31:48 +0100

    reformated the code and added functions to kick people from VCs

--- main.py
+++ main.py
@@ -35,6 +35,58 @@ def load_vc():
     print("Loading completed")
 
 
+def check_permanent_owner(user_id):
+    # Check if the User owns a permanent VC
+    owns_permanent_vc = False
+    voice_channel = None
+
+    for item in voice_channel_owners:
+        if user_id == item.get("User_ID") and item.get("Temp_VC") == "False":
+            owns_permanent_vc = True
+            vc_id = item.get("VC_Channel_ID")
+
+            voice_channel = discord.utils.get(bot.get_all_channels(),
+                                              id=int(vc_id),
+                                              type=discord.ChannelType.voice)
+
+            break
+
+    return owns_permanent_vc, voice_channel
+
+
+def getUserFromID(user_id):
+    for member in bot.get_all_members():
+        if member.id == int(user_id):
+            user = discord.utils.get(bot.get_all_members(), id=int(user_id))
+            return user
+    return None
+
+
+def getVCFromID(vc_id):
+    vc_channel_id = discord.utils.get(bot.get_all_channels(),
+                                      id=int(vc_id),
+                                      type=discord.ChannelType.voice)
+    return vc_channel_id
+
+
+def getModRights(user_id):
+    # Check if the User has a Moderator Role
+    mod_rights = False
+    for role in user_id.roles:
+        if role.id in config.MOD_ROLES:
+            mod_rights = True
+            break
+    return mod_rights
+
+
+def isUserBanned(user_id, vc_id):
+    for item in voice_channel_owners:
+        if vc_id == item.get("VC_Channel_ID"):
+            if user_id in item.get("Ban"):
+                return True
+    return False
+
+
 # After the Bot is ready
 @bot.event
 async def on_ready():
@@ -144,7 +196,8 @@ async def vc_create(ctx, name: typing.Optional[str] = None):
         "User_ID": ctx.author.id,
         "VC_Channel_ID": new_channel.id,
         "Last_Join": datetime.now().timestamp(),
-        "Temp_VC": "False"
+        "Temp_VC": "False",
+        "Ban": []
     }
     voice_channel_owners.append(entry)
 
@@ -163,26 +216,22 @@ async def vc_set_user_count(ctx, user_count: int):
     # Convert user_count to an integer if it's not
     user_count = int(user_count)
 
-    # Check if the User owns a permanent VC
-    owns_permanent_vc = False
-    vc_id = None
-
-    for item in voice_channel_owners:
-        if ctx.author.id == item.get("User_ID") and item.get("Temp_VC") == "False":
-            owns_permanent_vc = True
-            vc_id = item.get("VC_Channel_ID")
-            break
+    owns_permanent_vc, voice_channel = check_permanent_owner(ctx.author.id)
 
     if owns_permanent_vc:
-        vc_channel_id = discord.utils.get(bot.get_all_channels(), id=vc_id, type=discord.ChannelType.voice)
-
-        # Check if the channel is a VoiceChannel and then edit it
-        if isinstance(vc_channel_id, discord.VoiceChannel):
-            if user_count == 0:
-                await vc_channel_id.edit(user_limit=0)
-            else:
-                await vc_channel_id.edit(user_limit=user_count)
-            await ctx.respond(f"Changed permanent VC User Count", ephemeral=True)
+        if isinstance(voice_channel, discord.VoiceChannel):
+            # Check if the channel is a VoiceChannel and then edit it
+            if isinstance(voice_channel, discord.VoiceChannel):
+                if user_count == 0:
+                    await voice_channel.edit(user_limit=0)
+                    await ctx.respond(f"Removed permanent VC User Limit", ephemeral=True)
+                if user_count > 99:
+                    await voice_channel.edit(user_limit=99)
+                    await ctx.respond(f"User count can't be higher than 99, applied 99 as current limit",
+                                      ephemeral=True)
+                else:
+                    await voice_channel.edit(user_limit=user_count)
+                    await ctx.respond(f"Changed permanent VC User Count", ephemeral=True)
 
     else:
         await ctx.respond(f"You don't own any permanent VCs", ephemeral=True)
@@ -191,29 +240,15 @@ async def vc_set_user_count(ctx, user_count: int):
 # Slash command to change the name of a permanent VC
 @bot.command(description="Rename your VC")
 async def vc_rename(ctx, new_name):
-    # Search the user ID in the JSON with the owners
-
     # Check if the User owns a permanent VC
-    owns_permanent_vc = False
-    vc_id = None
-
-    for item in voice_channel_owners:
-        if ctx.author.id == item.get("User_ID") and item.get("Temp_VC") == "False":
-            owns_permanent_vc = True
-            vc_id = item.get("VC_Channel_ID")
-            break
+    owns_permanent_vc, voice_channel = check_permanent_owner(ctx.author.id)
 
     if owns_permanent_vc:
-        # When found, get the channel class and change the name
-        vc_channel_id = discord.utils.get(bot.get_all_channels(),
-                                          id=int(vc_id),
-                                          type=discord.ChannelType.voice)
-
-        if isinstance(vc_channel_id, discord.VoiceChannel):
-            await vc_channel_id.edit(name=new_name)
+        if isinstance(voice_channel, discord.VoiceChannel):
+            await voice_channel.edit(name=new_name)
             await ctx.respond(f"Changed permanent VC Name to {new_name}", ephemeral=True)
         else:
-            await ctx.respond(f"Channel with ID {vc_id} "
+            await ctx.respond(f"Channel with ID {voice_channel.id} "
                               f"is not a voice channel or does not exist.", ephemeral=True)
     else:
         await ctx.respond(f"You don't own a permanent VC", ephemeral=True)
@@ -222,27 +257,18 @@ async def vc_rename(ctx, new_name):
 # Slash command to delete an permanent VC
 @bot.command(description="Delete VC")
 async def vc_delete(ctx):
-
     # Check if the User owns a permanent VC
-    owns_permanent_vc = False
-    vc_id = None
-
-    for item in voice_channel_owners:
-        if ctx.author.id == item.get("User_ID") and item.get("Temp_VC") == "False":
-            owns_permanent_vc = True
-            vc_id = item.get("VC_Channel_ID")
-            break
+    owns_permanent_vc, voice_channel = check_permanent_owner(ctx.author.id)
 
     if owns_permanent_vc:
-        vc_channel_id = discord.utils.get(bot.get_all_channels(),
-                                          id=int(vc_id),
-                                          type=discord.ChannelType.voice)
-
-        await vc_channel_id.delete()
-        await ctx.respond(f"Permanent VC deleted", ephemeral=True)
-
+        if isinstance(voice_channel, discord.VoiceChannel):
+            await voice_channel.delete()
+            await ctx.respond(f"Deleted the voice channel", ephemeral=True)
+        else:
+            await ctx.respond(f"Channel with ID {voice_channel.id} "
+                              f"is not a voice channel or does not exist.", ephemeral=True)
     else:
-        await ctx.respond(f"You dont own an permanent VC", ephemeral=True)
+        await ctx.respond(f"You don't own a permanent VC", ephemeral=True)
 
 
 # Remove the VC Channel from the JSON, when the VC Channel is deleted
@@ -297,7 +323,6 @@ async def check_inactive_vcs():
         for item in voice_channel_owners:
             if (item.get("Temp_VC") == "False" and
                     (datetime.now().timestamp() - item.get("Last_Join", 0) > one_month_seconds)):
-
                 vc_channel_id = discord.utils.get(bot.get_all_channels(),
                                                   id=int(item["VC_Channel_ID"]),
                                                   type=discord.ChannelType.voice)
@@ -312,50 +337,79 @@ async def check_inactive_vcs():
 @bot.command(description="Only for Mods: Delete VC")
 async def vc_mod_delete(ctx, vc_id):
     # Check if the User has a Moderator Role
-    mod_rights = False
-    for role in ctx.author.roles:
-        if role.id in config.MOD_ROLES:
-            mod_rights = True
-            break
+    mod_rights = getModRights(ctx.author)
+    voice_channel = getVCFromID(vc_id)
 
-    if mod_rights:
-        # When found, get the channel class and delete it
-        vc_channel_id = discord.utils.get(bot.get_all_channels(),
-                                          id=int(vc_id),
-                                          type=discord.ChannelType.voice)
-
-        if isinstance(vc_channel_id, discord.VoiceChannel):
-            await vc_channel_id.delete()
-            await ctx.respond(f"Deleted the voice channel", ephemeral=True)
-        else:
-            await ctx.respond(f"Channel with ID {vc_id} is not a voice channel or does not exist.", ephemeral=True)
-
+    if mod_rights and isinstance(voice_channel, discord.VoiceChannel):
+        await voice_channel.delete()
+        await ctx.respond(f"Deleted the voice channel", ephemeral=True)
     else:
-        await ctx.respond(f"You don't have Moderator rights", ephemeral=True)
-
+        await ctx.respond(f"Channel with ID {vc_id} is not a voice channel or does not exist.", ephemeral=True)
 
 
 # Rename a permanent VC from another User
 @bot.command(description="Only for Mods: Rename VC")
 async def vc_mod_rename(ctx, vc_id, new_name):
     # Check if the User has a Moderator Role
-    mod_rights = False
-    for role in ctx.author.roles:
-        if role.id in config.MOD_ROLES:
-            mod_rights = True
+    mod_rights = getModRights(ctx.author)
+    voice_channel = getVCFromID(vc_id)
+
+    if mod_rights and isinstance(voice_channel, discord.VoiceChannel):
+        await voice_channel.edit(name=new_name)
+        await ctx.respond(f"Changed permanent VC Name to {new_name}", ephemeral=True)
+    else:
+        await ctx.respond(f">:c", ephemeral=True)
+
+
+# Kick a user from your permanent VC
+@bot.command(description="Kick User from VC")
+async def vc_kick(ctx, user_id):
+    # Check if the User owns a permanent VC
+    owns_permanent_vc = False
+    vc_id = None
+
+    for item in voice_channel_owners:
+        if ctx.author.id == item.get("User_ID") and item.get("Temp_VC") == "False":
+            owns_permanent_vc = True
+            vc_id = item.get("VC_Channel_ID")
             break
 
-    # Try to change the name of the VC
-    if mod_rights:
+    if owns_permanent_vc:
         vc_channel_id = discord.utils.get(bot.get_all_channels(),
                                           id=int(vc_id),
                                           type=discord.ChannelType.voice)
 
+        # Check if the channel is a VoiceChannel and then edit it
         if isinstance(vc_channel_id, discord.VoiceChannel):
-            await vc_channel_id.edit(name=new_name)
-            await ctx.respond(f"Changed permanent VC Name to {new_name}", ephemeral=True)
-        else:
-            await ctx.respond(f"Channel with ID {vc_id} is not a voice channel or does not exist.", ephemeral=True)
+            # Check if the user is in the VC
+            for member in vc_channel_id.members:
+                if member.id == int(user_id):
+                    await member.move_to(None)
+                    await ctx.respond(f"Kicked user from VC", ephemeral=True)
+                    return
+
+            await ctx.respond(f"User is not in the VC", ephemeral=True)
+
+    else:
+        await ctx.respond(f"You don't own a permanent VC", ephemeral=True)
+
+
+# Kick a user from a permanent VC
+@bot.command(description="Kick User a VC")
+async def vc_mod_kick(ctx, vc_id, user_id):
+    # Check if the User has a Moderator Role
+    mod_rights = getModRights(ctx.author)
+    voice_channel = getVCFromID(vc_id)
+
+    if mod_rights and isinstance(voice_channel, discord.VoiceChannel):
+        # Check if the user is in the VC
+        for member in voice_channel.members:
+            if member.id == int(user_id):
+                await member.move_to(None)
+                await ctx.respond(f"Kicked user from VC", ephemeral=True)
+                return
+
+        await ctx.respond(f"User is not in the VC", ephemeral=True)
 
     else:
         await ctx.respond(f"You don't have Moderator rights", ephemeral=True)
